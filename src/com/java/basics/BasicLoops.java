8c500df62ea668ec5355c816d2954d2acd1b0c63
sujithgoud2210 <sujith200322@gmail.com>
2026-01-23 20:32:23 +0530

    fourth commit

---
+++ src/com/java/basics/BasicLoops.java
@@ -0,0 +1,16 @@
+package com.java.basics;
+
+public class BasicLoops {
+    public static void main(String[] args) {
+        int a = 10;
+//        if(a==10){
+//            System.out.println("True");
+//        }
+        int i = 1;
+        while (i <= a) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
