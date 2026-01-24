4d88a74d2ddb1474f36ecbd41b9dfeab714ebec5
sujithgoud2210 <sujith200322@gmail.com>
2026-01-24 16:08:16 +0530

    fifth commit

---
+++ src/com/java/basics/Largest.java
@@ -0,0 +1,21 @@
+package com.java.basics;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a b c values");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
