5714af441ddae0b11fac75b6e1e845647ce6a883
sujithgoud2210 <sujith200322@gmail.com>
2026-02-06 15:47:10 +0530

    thirteen commit

--- src/com/java/binarysearch/Ceiling.java
+++ src/com/java/binarysearch/Ceiling.java
@@ -24,6 +24,9 @@ public class Ceiling {
 
     }
     static int ceiling(int[] arr,int target){
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
         int start=0;
         int end=arr.length-1;
         while(start<=end){
---
+++ src/com/java/bubblesort/BubbleSort.java
@@ -0,0 +1,4 @@
+package com.java.bubblesort;
+
+public class BubbleSort {
+}
