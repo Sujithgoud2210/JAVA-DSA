824df929ac894da7b170ea598955037ba22c95ae
sujithgoud2210 <sujith200322@gmail.com>
2026-04-02 23:53:55 +0530

    static example

--- src/com/java/oops/staticexample/Main.java
+++ src/com/java/oops/staticexample/Main.java
@@ -2,11 +2,13 @@ package com.java.oops.staticexample;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human(22, "sujith", 10000, false);
-        Human human1 = new Human(32, "sujith1", 11000, true);
-        Human human2 = new Human(24, "sujith2", 15000, false);
-        System.out.println(human1);
-        System.out.println(human1.population);
+//        Human human = new Human(22, "sujith", 10000, false);
+//        Human human1 = new Human(32, "sujith1", 11000, true);
+//        Human human2 = new Human(24, "sujith2", 15000, false);
+//        System.out.println(human1);
+        System.out.println(Human.population);
+        greet();
+        greeting();
     }
     static void greet(){
         System.out.println("hello");
